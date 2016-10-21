c9e4770cb05bcaf150893607d6d6464e0ca2b2f3
Ivan-Vshtd <1187804@gmail.com>
2016-10-21 10:37:08 +0300

    Add files via upload

---
+++ level11/lesson11/bonus02/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.test.level11.lesson11.bonus02;
+
+/* Нужно добавить в программу новую функциональность
+Добавь общий базовый класс к классам-фигур:  (фигуры из шахмат).
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+    }
+
+    public class Chessmate
+    {
+    }
+    public class King extends Chessmate
+    {
+    }
+
+    public class Queen extends Chessmate
+    {
+    }
+
+    public class Rook extends Chessmate
+    {
+    }
+
+    public class Knight extends Chessmate
+    {
+    }
+
+    public class Bishop extends Chessmate
+    {
+    }
+
+    public class Pawn extends Chessmate
+    {
+    }
+}
---
+++ level12/lesson04/task03/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.test.level12.lesson04.task03;
+
+/* Пять методов print с разными параметрами
+Написать пять методов print с разными параметрами.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public void print (String a)
+    {
+        System.out.println(a);
+    }
+    public void print (int a)
+    {
+        System.out.println(a);
+    }
+    public void print (float a)
+    {
+        System.out.println(a);
+    }
+    public void print (double a)
+    {
+        System.out.println(a);
+    }
+    public void print (char a)
+    {
+        System.out.println(a);
+    }//Напишите тут ваши методы
+}
